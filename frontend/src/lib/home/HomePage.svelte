973d8f4a3cf6ea78ea29f7ed7cfc158971ddfb9f
Frederick Santiago <frsantiago2001@gmail.com>
2024-09-01 14:16:20 -0400

    add image

--- frontend/src/lib/home/HomePage.svelte
+++ frontend/src/lib/home/HomePage.svelte
@@ -2,6 +2,8 @@
   import TopBar from "../Shared/TopBar.svelte";
   import HomeDescription from "./HomeDescription.svelte";
   import * as styles from '../Shared/styles/PageLayout.css'
+  // 1200 x 1600
+  import meInParisImg from '../../assets/Me_in_Paris.jpg'
 </script>
 
 <div class='md-layout'>
@@ -9,4 +11,5 @@
   <main>
     <HomeDescription />
   </main>
+  <img class='profileImg' src={meInParisImg} alt='Frederick in Paris'>
 </div>
